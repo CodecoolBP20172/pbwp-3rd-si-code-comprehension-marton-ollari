c6d4a1c1538f9fa611d391cc9d250dbb0fb9b4a8
Ollári Márton <ollarimarton@gmail.com>
2017-07-05 11:37:13 +0200

    Final comments

--- comprehension.py
+++ comprehension.py
@@ -1,33 +1,33 @@
 import random  # import random module
 # Program ask for name, and to guess a random number between 1 and 20 in 6 guesses.
-guessesTaken = 0  # assign 0 to variable guessesTaken
+guessesTaken = 0  # Assign 0 to variable guessesTaken
 
-print('Hello! What is your name?')  # print string
-myName = input()  # assign input to variable myName
+print('Hello! What is your name?')  # Print string to console
+myName = input()  # Assign input to variable myName
 
-number = random.randint(1, 20)  # assign random number between 1 and 20 to variable number
-print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')  # print string
+number = random.randint(1, 20)  # Assign random number between 1 and 20 to variable number
+print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')  # Print string to console
 
-while guessesTaken < 6:  # loop, while variable guessesTaken is smaller than 6
-    print('Take a guess.')  # print string
-    guess = input()  # assign input to variable guess
-    guess = int(guess)  # change input from string to integer
+while guessesTaken < 6:  # Loop, while variable guessesTaken is smaller than 6
+    print('Take a guess.')  # Print string to console
+    guess = input()  # Assign input to variable guess
+    guess = int(guess)  # Change input from string to integer
 
-    guessesTaken += 1  # add 1 to variable guessesTaken
+    guessesTaken += 1  # Add 1 to variable guessesTaken
 
-    if guess < number:  # check if guess smaller than number
-        print('Your guess is too low.')  # print string if line above True
+    if guess < number:  # Check if guess smaller than number
+        print('Your guess is too low.')  # Print string to console if line above True
 
-    if guess > number:  # check if guess bigger than number
-        print('Your guess is too high.')  # print string if line above True
+    if guess > number:  # Check if guess bigger than number
+        print('Your guess is too high.')  # Print string to console if line above True
 
-    if guess == number:  # check if guess equals number
-        break  # break while loop if line above True
+    if guess == number:  # Check if guess equals number
+        break  # Break while loop if line above True
 
-if guess == number:  # check if guess equals number
-    guessesTaken = str(guessesTaken)  # if line above True: change input from integer to string
-    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')  # print string
+if guess == number:  # Check if guess equals number
+    guessesTaken = str(guessesTaken)  # If line above True: change guessesTaken type from integer to string
+    print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')  # Print string to console
 
-if guess != number:  # check if guess not equals number 
-    number = str(number)  # if line above True: change input from integer to string
-    print('Nope. The number I was thinking of was ' + number)  # print string
+if guess != number:  # Check if guess not equals number
+    number = str(number)  # If line above True: change number type from integer to string
+    print('Nope. The number I was thinking of was ' + number)  # Print string to console
